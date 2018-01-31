7e84aa1e915538e2c3e65273d0a14bed98e69aeb
c0d3ster <cody.douglass@hotmail.com>
2018-01-30 22:51:55 -0500

    classnames for conditional button display and video inserted

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import Observed from 'react-observed';
+import classnames from 'classnames';
 import Section from './SectionContainer.jsx';
 import content from '../data/content';
 
 export default class AboutMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count : 0 };
+  }
+
+  nextItem() {
+    this.setState({ count : this.state.count + 1});
+  }
+
+  prevItem() {
+    this.setState({ count : this.state.count - 1});
+  }
   render() {
     let navList = ["About Me", "Leadership", "Projects", "Gallery", "Contact"];
+    let contentList = ["who", "childhood", "teenage", "college"];
+    let backgroundList = ["","./VirginiaBeach.jpg"];
+    let currItem = contentList[this.state.count];
+    let prevButton = classnames("about-prev-button", "col", {
+      'hidden': this.state.count == 0
+    });
+    let nextButton = classnames("about-next-button", "col", {
+      'hidden': this.state.count == contentList.length-1
+    });
     return (
       <Section background="./VirginiaBeach.jpg" title="Cody Douglass" navList={navList}>
         <div className="row">
+          <button className={prevButton} onClick={this.prevItem.bind(this)}>prev</button>
           <h2 className="col center"> Who Is Cody Douglass?</h2>
+          <button className={nextButton} onClick={this.nextItem.bind(this)}>next</button>
         </div>
         <div className="row">
-          <p className="about-content">{content.who}</p>
-          <p className="about-content">{content.childhood}</p>
-          <p className="about-content">{content.teenage}</p>
-          <p className="about-content">{content.college}</p>
+          <p className="col">{content[currItem]}</p>
         </div>
-        {/*<iframe className="about-video"
-          src="https://www.youtube.com/embed/P7G_YggEP4I?rel=0?showinfo=0"
+        <iframe className="about-video"
+          src="https://www.youtube.com/embed/fdzHO2bFIcc"
           frameBorder="0"
           allow="encrypted-media"
           allowFullScreen>
-        </iframe>*/}
+        </iframe>
       </Section>
     );
   }
