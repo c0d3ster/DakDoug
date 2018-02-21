954749507307abb0917bf261a7c3f4cd8812ca39
c0d3ster <cody.douglass@hotmail.com>
2018-02-21 03:51:34 -0500

    landing page added for block one

--- src/components/blockone/BlockOne.jsx
+++ src/components/blockone/BlockOne.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BlockOneLandingContainer from './BlockOneLandingContainer.jsx';
 import EOSBlockExplorer from './EOSBlockExplorer.jsx';
 import SteemItCondenser from './SteemItCondenser.jsx';
 import Footer from '../Footer.jsx';
@@ -6,6 +7,7 @@ import Footer from '../Footer.jsx';
 /* Create functional stateless components whenever possible, the BlockOne component shouldn't need to manage the child components' states */
 const BlockOne = () => (
   <div>
+    <BlockOneLandingContainer/>
     <EOSBlockExplorer/>
     <SteemItCondenser/>
     <Footer/>
